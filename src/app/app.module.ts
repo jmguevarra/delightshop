e5b7da84819f47c6be926f93732a12f687407542
jmguevarra <jaimeguevarra22@gmail.com>
2022-05-04 16:01:28 +1000

    Fixing Bug in missing added recipe after moving in Shoppint list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { AppRoutingModule } from './app-routing-module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipeService } from './recipes/recipe.service';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [IngredientService],
+  providers: [IngredientService, RecipeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
